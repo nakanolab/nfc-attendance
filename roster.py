c7e41024dc13049a462bfd8e3a813a12b98326a0
Jun Nakano <nakanoj@gmail.com>
2020-04-01 14:48:54 +0900

    Added logging.

--- roster.py
+++ roster.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 import csv
+import datetime
+import logging
 import string
 
 RISYU_FILE = 'risyu.csv'
@@ -11,6 +13,8 @@ class Roster:
         self.present = set()
 
     def set_course_code(self, course_code):
+        setup_logging(course_code)
+        self.logger = logging.getLogger()
         self.course_code = course_code
 
     @property
@@ -19,13 +23,25 @@ class Roster:
 
     def check_in(self, student_id):
         if student_id not in self.students:
+            self.logger.warning('unregistered student: %s', student_id)
             return False, f'未登録の学生\n{student_id}'
         elif student_id in self.present:
+            self.logger.warning('already checked in: %s', student_id)
             return False, f'チェックイン済み\n{student_id}'
         else:
             self.present.add(student_id)
             student_class_no, student_name = self.students[student_id]
+            self.logger.info('added %s %s %s',
+                             student_id, student_class_no, student_name)
             return True, f'{student_class_no}\n{student_name}'
+
+    def replay_log(self):
+        # TODO: return a list of student IDs who checked in
+        return []
+
+    def report_absent_students(self):
+        # TODO: log absent students
+        pass
 
 
 def load_risyu(filename):
@@ -47,6 +63,22 @@ def load_risyu(filename):
             courses[course_code] = course_name
             roster[course_code][student_id] = (student_class_no, student_name)
     return courses, roster
+
+def get_log_filename(course_code):
+    date = datetime.date.today().strftime('%Y-%m-%d')
+    return f'log/attendance-{course_code}-{date}.log'
+
+def setup_logging(course_code):
+    format_ = '%(asctime)s %(name)-8s %(levelname)-8s %(message)s'
+    logging.basicConfig(level=logging.INFO,
+                        format=format_, datefmt='%m/%d %H:%M',
+                        filename=get_log_filename(course_code), filemode='a')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter(format_)
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+    logging.getLogger('nfc.clf').setLevel(logging.WARNING)  # to silence NFC log
 
 def dump():
     courses, roster = load_risyu('risyu.csv')
